62cb4c351722bd51f2cfd087183e5acd3971e800
Santhosh Raj <118260530+SanthoshRaj4935@users.noreply.github.com>
2022-11-24 12:42:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+            add(ref i,78);
+            Console.WriteLine("i={0}", i);
+            Console.ReadKey();
+
+        }
+        static void add(ref int a , int b)
+        {
+            a = 10;
+            int c = a + b;
+            Console.WriteLine("a+b={0}", c);
+        }
+    }
+}
